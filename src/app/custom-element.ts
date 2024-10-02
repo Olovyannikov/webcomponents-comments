80fe7d4a9beb89390c637414dd8c083f32502556
Ilya Olovyannikov <excaliburgyro@gmail.com>
2024-10-02 10:56:50 +0300

    feat(api): add custom element define decorator

---
+++ src/app/custom-element.ts
@@ -0,0 +1,24 @@
+/**
+ * Декоратор, который регистрирует кастомный HTML element.
+ */
+export function CustomElement(selector: string, options?: ElementDefinitionOptions): CustomElementDecorator {
+    return (_target, context) => {
+        context.addInitializer(function () {
+            customElements.define(selector, this, options);
+        });
+    };
+}
+
+/**
+ * Кастомный декоратор для элемента. Основан на {@link ClassDecorator}.
+ */
+type CustomElementDecorator = <E extends HTMLElement, C extends CustomElementConstructor<E>>(target: C, context: ClassDecoratorContext<C>) => C | void;
+
+/**
+ * Ссылка на конкретный кастомный класс элемента;
+ */
+type CustomElementConstructor<E extends HTMLElement> = new (...args: any[]) => E;
+
+export function defineCustomElement(ctor: CustomElementConstructor<HTMLElement>, selector: string, options?: ElementDefinitionOptions): void {
+    customElements.define(selector, ctor, options);
+}
