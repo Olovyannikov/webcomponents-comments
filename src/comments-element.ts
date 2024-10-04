4e2bca54622ec2faeb83cb4c80f8d824b62e8756
Ilya Olovyannikov <excaliburgyro@gmail.com>
2024-10-04 16:12:28 +0300

    feat(root): init shadow root

--- src/comments-element.ts
+++ src/comments-element.ts
@@ -1,10 +1,11 @@
-import {CommentsOptions} from "./options";
+import type {CommentsOptions} from "./options";
 import {defineCustomElement} from "./common/custom-element.ts";
 import {CommentViewModel} from "./view-model/comment-view-model.ts";
 import {ElementEventHandler} from "./element-event-handler.ts";
 import {CommentSorter} from "./view-model/comment-sorter.ts";
 import {SortKey} from "./options/misc.ts";
 import {SpinnerFactory} from "./thread/basic/spinner-factory.ts";
+import {STYLE_SHEET} from "./css/stylesheet.ts";
 
 export class CommentsElement extends HTMLElement {
     private container!: HTMLElement;
@@ -19,6 +20,20 @@ export class CommentsElement extends HTMLElement {
     #currentSortKey!: SortKey;
     #connected: boolean = false;
     #dataFetched: boolean = false;
+
+    constructor() {
+        super();
+        this.#initShadowDom(this.attachShadow({mode: 'open'}));
+    }
+
+    #initShadowDom(shadowRoot: ShadowRoot): void {
+        shadowRoot.innerHTML = `
+            <section id="comments-container">
+            </section>
+        `;
+        shadowRoot.adoptedStyleSheets = [STYLE_SHEET];
+        this.container = shadowRoot.querySelector<HTMLElement>('#comments-container')!;
+    }
 }
 
 defineCustomElement(CommentsElement, 'ithub-comments');
--- src/options/index.ts
+++ src/options/index.ts
@@ -2,7 +2,8 @@ import type {CurrentUser} from "./current-user.ts";
 import type {Icons} from "./icons.ts";
 import type {Labels} from "./labels.ts";
 import type {Misc} from "./misc.ts";
-import {Functionalities} from "./functionalities.ts";
+import type {Functionalities} from "./functionalities.ts";
+import type {Formatters} from "./formatters.ts";
 
-export interface CommentsOptions extends CurrentUser, Icons, Labels, Misc, Functionalities {
+export interface CommentsOptions extends CurrentUser, Icons, Labels, Misc, Functionalities, Formatters {
 }
